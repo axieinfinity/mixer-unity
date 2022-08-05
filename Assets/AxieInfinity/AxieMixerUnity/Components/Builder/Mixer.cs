efc2e4be8b9a1caf293312baeae8b5e383f4c1b0
phuong-axie <phuong.nguyen@skymavis.com>
2022-08-05 23:16:30 +0700

    Hard code to fix not working on some pc

--- Assets/AxieInfinity/AxieMixerUnity/Components/Builder/Mixer.cs
+++ Assets/AxieInfinity/AxieMixerUnity/Components/Builder/Mixer.cs
@@ -108,8 +108,11 @@ namespace AxieMixer.Unity
             {
                 Dictionary<string, Texture2D> textures = new Dictionary<string, Texture2D>();
                 var atlasAsset = Resources.Load<TextAsset>($"{StuffName}/{quality}/axie-2d-v3-stuff");
-                string[] lines = atlasAsset.text.Split('\n').Where(x => x.Contains(".png")).Select(x => x.Replace(".png", "")).ToArray();
-                foreach (var texName in lines)
+
+                //CHEAT - phuongnk: this may contains trash character on random devices. Hard code for now!
+                //string[] lines = atlasAsset.text.Split('\n').Where(x => x.Contains(".png")).Select(x => x.Replace(".png", "")).ToArray();
+                string texName = "axie-2d-v3-stuff";
+                //foreach (var texName in lines)
                 {
                     var colorTexture = Resources.Load<Texture2D>($"{StuffName}/{quality}/{texName}_color");
                     colorTexture.name = texName;
